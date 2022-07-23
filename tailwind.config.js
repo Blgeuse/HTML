6ffa0402fd9d7a7bb15ae429cc0455fe7b65f7e6
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-07-23 15:29:49 +0300

    Adapt main page for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+      'tablet': '600px',
+      'desktop': '1200px',
+      'portrait': {'raw': '(orientation: portrait)'},
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'model-3': "url('./../img/m3.jpg')",
+        'mobile-model-3': "url('./../img/mobile-m3.jpg')",
+      }),
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
