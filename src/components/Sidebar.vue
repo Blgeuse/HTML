94815964b5996729aa9504e1bf23f64729b66789
Blgeuse <akhmed.r4mazanov@yandex.ru>
2022-07-26 20:24:30 +0300

    added header

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-full transition bg-white fixed w-[250px] p-[20px]">
+    <!-- <router-link> </router-link> -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header
+    class="flex items-center relative shadow-sm z-[1] px-5 font-bold bg-white h-[64px]"
+  >
+    <router-link :to="{ name: 'home' }">Ui kit</router-link>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
